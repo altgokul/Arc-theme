6328df90a856402b8abe4b3898200a217594584f
gokul <alt.gokul@gmail.com>
2015-09-06 22:46:05 +0530

    removed unwanted things

--- install.sh
+++ install.sh
@@ -1,3 +1,10 @@
 #/usr/bin/env bash
-./autogen.sh --prefix=/usr --disable=gnome-shell --disable-metacity --disable-cinnamon --disable-darker --disable-dark
-
+dir=$(cd `dirname $0`;pwd)
+cd $dir/common/gtk-3.0/3.18
+gulp
+cd $dir
+./autogen.sh --prefix=/usr --disable-gnome-shell --disable-metacity --disable-cinnamon --disable-darker --disable-dark
+sudo make install
+echo setting the theme
+gsettings reset org.gnome.desktop.interface gtk-theme;
+gsettings set org.gnome.desktop.interface gtk-theme "Arc"
